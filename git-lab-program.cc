f52c0e0a26f95bfe10c941971187aa234d98e38e
Connor Petty <cp944317@ohio.edu>
2023-05-19 00:22:53 -0400

    done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Connor Petty
+ *        Date: 5/19/2023
+ * Description: Hello git program.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
